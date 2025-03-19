c5e889c870081f286acf66d00752c0df0f04c990
tgschindler1@gmail.com <tgschindler1@gmail.com>
2025-03-18 20:05:22 -0400

    m09 exercises

---
+++ demo/jabber.py
@@ -0,0 +1,3 @@
+def tell_me_something():
+    print("something")
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    url='https://github.com/tgschindler1/m09',
+    author='Thomas Schindler',
+    author_email='tgschindler1@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
